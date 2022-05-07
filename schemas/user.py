0256a9a10cd67a8c21e0e5c9106c89c3de30ab67
Andres Alvarez <andres15alvarez@gmail.com>
2022-05-07 18:32:24 -0400

    Add examples and improve readbility

--- schemas/user.py
+++ schemas/user.py
@@ -1,16 +1,33 @@
-from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserResponse(BaseModel):
-    active: bool
-    first_name: str
-    last_name: str
-    email: EmailStr
+    active: bool = Field(True, description='If the user is active')
+    first_name: str = Field(None, description='First name of the user', max_length=50)
+    last_name: str = Field(None, description='Last name of the user', max_length=50)
+    email: EmailStr = Field(description='Email of the user')
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'active': True,
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'email': 'johndoe@email.com'
+            }
+        }
 
 
 class UserRequest(UserResponse):
-    active: Optional[bool] = True
-    password: str
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
+    password: str = Field(description='Password of the user', max_length=255, min_length=8)
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'active': True,
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'email': 'johndoe@email.com',
+                'password': 'password'
+            }
+        }
