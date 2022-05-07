1884f6185d4b3c97e24c60d5572ec8a546a4410d
Andres Alvarez <andres15alvarez@gmail.com>
2022-05-07 18:31:22 -0400

    Create method to verify password

--- utils/crypt.py
+++ utils/crypt.py
@@ -12,3 +12,16 @@ def hash_password(password: str) -> str:
 
     """
     return hashlib.sha256(password.encode()).hexdigest()
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    """Verify if the password is the same after hashing as the password hashed in the database.
+
+    Args:
+        - password (str): raw password.
+        - hashed_password (str): password hashed.
+
+    Returns:
+        - bool: if the password is valid.
+    """
+    return hashed_password == hash_password(password)
