010e604828577228e543b0fbf89e6ba19c2ae926
Andres Alvarez <andres15alvarez@gmail.com>
2022-05-07 18:29:46 -0400

    Add constants for jwt

--- settings/base.py
+++ settings/base.py
@@ -1,15 +1,20 @@
+from datetime import timedelta
 import os
 from pathlib import Path
 import dotenv
 
 
 dotenv.load_dotenv()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 ENV = os.environ.get('ENV' 'development')
 DEBUG = eval(os.environ.get('DEBUG', 'True'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 PORT = int(os.environ.get('PORT', '5000'))
+
 
 # Database
 DATABASE_HOST = os.environ.get('DATABASE_HOST')
@@ -20,3 +25,8 @@ DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 
 
 TIME_ZONE = os.environ.get('TIME_ZONE', 'UTC')
+
+
+# JWT
+JWT_LIFE_TIME = timedelta(hours=int(os.environ.get('JWT_LIFE_TIME', 24)))
+JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM', 'HS256')
