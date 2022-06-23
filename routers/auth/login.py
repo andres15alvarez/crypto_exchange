2d15950efc4eefa3dd2763225907bbba98603304
Andres Alvarez <andres15alvarez@gmail.com>
2022-06-22 20:27:05 -0400

    Improve login service

--- routers/auth/login.py
+++ routers/auth/login.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
+from exceptions.auth import AuthError
+from exceptions.not_found import UserNotFound
 
 from schemas.auth import LoginRequest, LoginResponse
 from utils import auth
@@ -27,7 +29,13 @@ def create_token(login: LoginRequest):
     Returns:
         - LoginResponse: token as string.
     """
-    if not auth.verify_login_user(login.email, login.password):
+    try:
+        is_user_logged, user = auth.verify_login_user(login.email, login.password)
+    except AuthError as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    except UserNotFound as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    if not is_user_logged:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
     token = auth.create_access_token(login.email)
-    return {'token': token}
+    return LoginResponse(token=token, id=user.id)
