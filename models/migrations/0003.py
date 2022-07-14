43b37c89222f15e6c970f8eebb7515cb71b91fd8
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-14 16:29:20 -0400

    Create migration for currency

---
+++ models/migrations/0003.py
@@ -0,0 +1,20 @@
+from pony.orm import Database
+import utils.database as database
+
+
+def migrate(db: Database):
+    query = """
+        CREATE TABLE IF NOT EXISTS public."currency"
+        (
+            id serial NOT NULL,
+            name character varying(100) NOT NULL,
+            symbol character varying(10) NOT NULL UNIQUE,
+            CONSTRAINT currency_pkey PRIMARY KEY (id)
+        );
+    """
+
+    db.execute(query)
+
+
+if __name__ == "__main__":
+    migrate(database.db)
