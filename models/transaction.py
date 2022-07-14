262b05f2b0f35bbfbdb9165b77491c7495dc784a
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-14 16:29:05 -0400

    Create transaction model

---
+++ models/transaction.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from decimal import Decimal
+from pony import orm
+from models.currency import Currency
+from models.user import User
+from utils.database import db
+
+
+class Transaction(db.Entity):
+    __table__ = "transaction"
+
+    user = orm.Required(User)
+    from_currency = orm.Required(Currency, reverse="from_transactions")
+    to_currency = orm.Required(Currency, reverse="to_transactions")
+    exchange_rate = orm.Required(Decimal, precision=20, scale=10)
+    created_at = orm.Required(datetime, default=datetime.now)
