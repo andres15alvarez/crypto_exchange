934006fdd47f984997e42f24d5c5925411e66648
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 14:32:25 -0400

    Fix migration

--- models/migrations/0004.py
+++ models/migrations/0004.py
@@ -7,14 +7,14 @@ def migrate(db: Database):
         CREATE TABLE IF NOT EXISTS public."transaction"
         (
             id serial NOT NULL,
-            user integer NOT NULL,
+            "user" integer NOT NULL,
             from_currency integer NOT NULL,
             to_currency integer NOT NULL,
             exchange_rate numeric(20, 10) NOT NULL,
             quantity numeric(20, 10) NOT NULL,
             created_at timestamp,
             CONSTRAINT transaction_pkey PRIMARY KEY (id),
-            CONSTRAINT transaction_user_fkey FOREIGN KEY (user) REFERENCES public."user"(id),
+            CONSTRAINT transaction_user_fkey FOREIGN KEY ("user") REFERENCES public."user"(id),
             CONSTRAINT transaction_from_currency_fkey FOREIGN KEY (from_currency) REFERENCES public."currency"(id),
             CONSTRAINT transaction_to_currency_fkey FOREIGN KEY (to_currency) REFERENCES public."currency"(id)
         );
