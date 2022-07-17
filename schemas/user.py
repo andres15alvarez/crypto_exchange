279e39c8139b83723888860e4c772e3860268f5e
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 14:13:25 -0400

    Update user schemas

--- schemas/user.py
+++ schemas/user.py
@@ -2,12 +2,11 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr, Field
 
 
-class UserResponse(BaseModel):
+class UserBase(BaseModel):
     active: bool = Field(True, description="If the user is active")
     first_name: str = Field(None, description="First name of the user", max_length=50)
     last_name: str = Field(None, description="Last name of the user", max_length=50)
     email: EmailStr = Field(description="Email of the user")
-    created_at: datetime = Field(description="Datetime when object was created")
 
     class Config:
         schema_extra = {
@@ -16,12 +15,26 @@ class UserResponse(BaseModel):
                 "first_name": "John",
                 "last_name": "Doe",
                 "email": "johndoe@email.com",
-                "created_at": "2022-06-23 16:00:00",
+            }
+        }
+
+class UserResponse(UserBase):
+    created_at: datetime = Field(description="Datetime when object was created")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "active": True,
+                "created_at": "2022-07-17T15:40:00",
+                "first_name": "John",
+                "last_name": "Doe",
+                "email": "johndoe@email.com",
+                "password": "password",
             }
         }
 
 
-class UserRequest(UserResponse):
+class UserRequest(UserBase):
     password: str = Field(
         description="Password of the user", max_length=255, min_length=8
     )
