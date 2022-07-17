8563594ef79f0808c3039a14332ab6455988bf0a
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 14:12:44 -0400

    Create currency schema

---
+++ schemas/currency.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+
+class CurrencyResponse(BaseModel):
+    name: str = Field(description="Cryptocurrency name")
+    symbol: str = Field(description="Cryptocurrency symbol")
+
+    class Config:
+        schema_extra = {
+            "example": {"symbol": "BTC", "name": "bitcoin"}
+        }
