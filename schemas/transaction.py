0c0bf04f9dd25c86c206c3f5561262b08603edc4
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 14:16:41 -0400

    Add validator for nested schemas

--- schemas/transaction.py
+++ schemas/transaction.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from decimal import Decimal
-from pydantic import BaseModel, Field
-
-from schemas.user import UserResponse
+from pydantic import BaseModel, Field, validator
+from models.currency import Currency
+from schemas.currency import CurrencyResponse
 
 
 class TransactionRequest(BaseModel):
@@ -21,24 +21,42 @@ class TransactionRequest(BaseModel):
 
 
 class TransactionResponse(TransactionRequest):
-    user: UserResponse = Field(description="User whom exchange the currency")
     exchange_rate: Decimal = Field(description="Exchange rate")
+    from_currency: CurrencyResponse = Field(description="Currency exchanged")
+    to_currency: CurrencyResponse = Field(description="Currency received")
     created_at: datetime = Field(description="Datetime when the transaction was made")
 
     class Config:
         schema_extra = {
             "example": {
-                "from_currency": "USD",
-                "to_currency": "BTC",
-                "quantity": 20_000.00,
-                "user": {
-                    "active": True,
-                    "first_name": "John",
-                    "last_name": "Doe",
-                    "email": "johndoe@email.com",
-                    "password": "password",
+                "from_currency": {
+                    "name": "dolar",
+                    "symbol": "USD"
                 },
+                "to_currency": {
+                    "name": "bitcoin",
+                    "symbol": "BTC"
+                },
+                "quantity": 20_000.00,
                 "exchange_rate": 0.00005,
-                "created_at": "2022-06-23 16:00:00",
+                "created_at": "2022-07-17T12:44:33.137017",
             }
         }
+
+    @validator("from_currency", pre=True)
+    def convert_from_currency_to_dict(cls, from_currency: int):
+        if isinstance(from_currency, dict):
+            return from_currency
+        if not isinstance(from_currency, int):
+            raise ValueError("from_currency must be an integer")
+        from_currency = Currency.get(id=from_currency)
+        return from_currency.to_dict()
+
+    @validator("to_currency", pre=True)
+    def convert_to_currency_to_dict(cls, to_currency: int):
+        if isinstance(to_currency, dict):
+            return to_currency
+        if not isinstance(to_currency, int):
+            raise ValueError("to_currency must be an integer")
+        to_currency = Currency.get(id=to_currency)
+        return to_currency.to_dict()
