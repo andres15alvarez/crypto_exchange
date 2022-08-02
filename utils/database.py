bc3fd502fbf947f463f7b4bcf9d526a499861345
Andres Alvarez <andres15alvarez@gmail.com>
2022-08-01 20:14:36 -0400

    Use a sqlite memory database for testing

--- utils/database.py
+++ utils/database.py
@@ -1,11 +1,17 @@
 from pony import orm
 import settings
 
-
-db = orm.Database(
-    provider="postgres",
-    user=settings.DATABASE_USER,
-    password=settings.DATABASE_PASSWORD,
-    host=settings.DATABASE_HOST,
-    database=settings.DATABASE_NAME,
-)
+if settings.ENV != 'test':
+    db = orm.Database(
+        provider="postgres",
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        database=settings.DATABASE_NAME,
+    )
+else:
+    db = orm.Database(
+        provider="sqlite",
+        filename=":memory:",
+        create_db=True
+    )
