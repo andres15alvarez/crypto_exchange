d8cc621a72be7cf1b683b7c3ee24194406e12fc6
Andres Alvarez <andres15alvarez@gmail.com>
2022-08-01 20:17:13 -0400

    Create mocks for services

--- tests/test_currency_router.py
+++ tests/test_currency_router.py
@@ -1,10 +1,16 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from routers.currency.list import Currency
 from main import app
+
 
 client = TestClient(app)
 
 
-def test_list_currencies():
+def test_list_currencies(mocker, db_session):
+    mocker.patch("routers.currency.list.db_session", return_value=db_session)
+    mocker.patch.object(Currency, "to_dict", return_value={"name": "bitcoin", "symbol": "BTC"})
+    mocker.patch("routers.currency.list.select", return_value=[Currency])
     response = client.get("/v1/currency")
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [{"name": "bitcoin", "symbol": "BTC"}]
--- tests/test_exchange_rate_router.py
+++ tests/test_exchange_rate_router.py
@@ -1,11 +1,14 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from routers.exchange.rate import coin_api
 from main import app
 
 client = TestClient(app)
 
 
-def test_list_currencies():
+def test_list_currencies(mocker, db_session):
     params = {"from_currency": "BTC", "to_currency": "USD"}
+    mocker.patch.object(coin_api, "exchange_rates_get_specific_rate", return_value={"rate": 1})
     response = client.get("/v1/exchange/rate", params=params)
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"from_currency": "BTC", "to_currency": "USD", "rate": 1}
